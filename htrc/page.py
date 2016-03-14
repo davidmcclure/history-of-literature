7baf45d86e331186ddcc120a6bb5aea2d9fb9918
David McClure <davidwilliammcclure@gmail.com>
2016-03-14 16:02:51 -0700

    Collapse the POS tags, for now.

--- htrc/page.py
+++ htrc/page.py
@@ -22,19 +22,13 @@ class Page:
     def token_counts(self):
 
         """
-        Generate a set of flat (type, POS, count) 3-tuples.
+        Generate a set of flat (token, count) tuples.
 
-        Yields: (token, POS, count)
+        Yields: (token, count)
         """
 
         for token, pc in self.json['body']['tokenPosCount'].items():
-            for pos, count in pc.items():
-
-                yield dict(
-                    token=token,
-                    pos=pos,
-                    count=count,
-                )
+            yield (token, sum(pc.values()))
 
 
     @property
@@ -43,18 +37,8 @@ class Page:
         """
         Generate a set of edge weight contributions.
 
-        Yields: ((token1, POS), (token2, POS), count)
+        Yields: (token1, token2, count)
         """
 
-        for tc1, tc2 in combinations(self.token_counts, 2):
-
-            yield dict(
-
-                token1=(tc1['token'], tc1['pos']),
-                token2=(tc2['token'], tc2['pos']),
-
-                # Take the smaller of the counts, to capture the number of
-                # _pairs_ of the two words that appear together.
-                count=min(tc1['count'], tc2['count'])
-
-            )
+        for (t1, c1), (t2, c2) in combinations(self.token_counts, 2):
+            yield (t1, t2, c1+c2)
