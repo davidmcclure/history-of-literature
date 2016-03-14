48275488aaa89954f23861c7286a31464630f9b9
David McClure <davidwilliammcclure@gmail.com>
2016-03-14 15:22:06 -0700

    Block in a Page#edges method, which generates (token1, token2, count) weight contributions.

--- htrc/page.py
+++ htrc/page.py
@@ -1,5 +1,8 @@
 
 
+from itertools import combinations
+
+
 class Page:
 
 
@@ -19,16 +22,39 @@ class Page:
     def token_counts(self):
 
         """
-        Get a flat list of type,POS,count 3-tuples.
+        Generate a set of flat (type, POS, count) 3-tuples.
 
-        Returns:
-            list [(type, pos, count), ...]
+        Yields: (token, POS, count)
         """
-
-        counts = []
 
         for token, pc in self.json['body']['tokenPosCount'].items():
             for pos, count in pc.items():
-                counts.append((token, pos, count))
 
-        return counts
+                yield dict(
+                    token=token,
+                    pos=pos,
+                    count=count,
+                )
+
+
+    @property
+    def edges(self):
+
+        """
+        Generate a set of edge weight contributions.
+
+        Yields: ((token1, POS), (token2, POS), count)
+        """
+
+        for tc1, tc2 in combinations(self.token_counts, 2):
+
+            yield dict(
+
+                token1=(tc1['token'], tc1['pos']),
+                token2=(tc2['token'], tc2['pos']),
+
+                # Take the smaller of the counts, to capture the number of
+                # _pairs_ of the two words that appear together.
+                count=min(tc1['count'], tc2['count'])
+
+            )
