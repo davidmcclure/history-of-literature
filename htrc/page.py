a49f7ebd1b89b6e5d0cde517f79a55be2e0a51e2
David McClure <davidwilliammcclure@gmail.com>
2016-03-14 14:31:46 -0700

    Stub in a page class.

---
+++ htrc/page.py
@@ -0,0 +1,15 @@
+
+
+class Page:
+
+
+    def __init__(self, json):
+
+        """
+        Wrap an individual page dict.
+
+        Args:
+            json (dict)
+        """
+
+        self.json = json
