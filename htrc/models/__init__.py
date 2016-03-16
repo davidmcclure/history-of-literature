41bea9fa3ac9e0ad615be60cc9c5def3c3d55578
David McClure <davidwilliammcclure@gmail.com>
2016-03-16 10:42:11 -0700

    Create a top-level Session class.

--- htrc/models/__init__.py
+++ htrc/models/__init__.py
@@ -1,9 +1,12 @@
 
 
 from sqlalchemy import create_engine
+from sqlalchemy.org import sessionmaker
 
 from .edge import Edge
 
 
-# TODO|dev
+# TODO: Read from config.
 engine = create_engine('postgresql://localhost/htrc', echo=True)
+
+Session = sessionmaker(bind=engine)
