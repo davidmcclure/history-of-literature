b7e83251a14f2b598848bd6786b3c28491810442
David McClure <davidwilliammcclure@gmail.com>
2016-03-28 15:20:36 -0700

    Implement the CountData#index() method.

--- htrc/count_data.py
+++ htrc/count_data.py
@@ -1,11 +1,14 @@
 
 
 import os
+import shelve
 
 from multiprocessing import Pool
+from contextlib import contextmanager
 
 from htrc.corpus import Corpus
 from htrc.volume import Volume
+
 
 
 class CountData:
@@ -21,6 +24,50 @@ class CountData:
         """
 
         self.path = os.path.abspath(path)
+
+        # Ensure the directory exists.
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+
+
+    @contextmanager
+    def get_shelf(self, year):
+
+        """
+        Get a shelve data handle.
+
+        Args:
+            year (int)
+
+        Yields: shelve.DbFilenameShelf
+        """
+
+        path = os.path.join(self.path, str(year))
+
+        with shelve.open(path) as data:
+            yield data
+
+
+    def update_year(self, year, counts):
+
+        """
+        Merge counts into a year shelf.
+
+        Args:
+            year (int)
+            counts (Counter)
+        """
+
+        with self.get_shelf(year) as data:
+            for token, count in counts.items():
+
+                # Update the count if the token has been seen.
+                if token in data:
+                    data[token] += count
+
+                # Otherwise, initialize the counter.
+                else:
+                    data[token] = count
 
 
     def index(self, numprocs=8):
@@ -40,7 +87,9 @@ class CountData:
             jobs = pool.imap(get_vol_counts, corpus.paths())
 
             for i, (year, counts) in enumerate(jobs):
-                print(year, len(counts))
+                self.update_year(year, counts)
+                print(i)
+
 
 
 def get_vol_counts(path):
