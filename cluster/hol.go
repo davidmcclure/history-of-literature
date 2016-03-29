35fbebd5a27b5a0dbf58caf00fdf9ec3d03cd2e3
David McClure <davidwilliammcclure@gmail.com>
2016-03-29 16:00:34 -0700

    Iterate through volume paths.

--- cluster/hol.go
+++ cluster/hol.go
@@ -3,30 +3,41 @@ package main
 import (
 	"github.com/jawher/mow.cli"
 	"os"
+	"path/filepath"
 )
 
 func main() {
 
 	app := cli.App("hol", "History of 'literature'")
 
-	app.Command(
-		"counts",
-		"Extract per-year token counts",
-		func(cmd *cli.Cmd) {
+	app.Command("counts", "Extract token counts", func(cmd *cli.Cmd) {
 
-			var corpus = cmd.StringArg(
-				"CORPUS",
-				"path/to/htrc",
-				"The HTRC corpus root",
-			)
+		var corpus = cmd.StringArg(
+			"CORPUS",
+			"path/to/htrc",
+			"HTRC root",
+		)
 
-			cmd.Action = func() {
-				println(*corpus)
-			}
+		cmd.Action = func() {
+			extractCounts(*corpus)
+		}
 
-		},
-	)
+	})
 
 	app.Run(os.Args)
+
+}
+
+func extractCounts(path string) {
+	filepath.Walk(path, walkVolume)
+}
+
+func walkVolume(path string, info os.FileInfo, err error) error {
+
+	if !info.IsDir() {
+		println(path)
+	}
+
+	return nil
 
 }
