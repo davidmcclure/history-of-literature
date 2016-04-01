4e3ef2ad1903d9d04f63e318935bc64043ed42ed
David McClure <davidwilliammcclure@gmail.com>
2016-04-01 13:39:21 -0700

    Bump the .editorconfig max_line_length -> 100.

--- cluster/htrc/corpus.go
+++ cluster/htrc/corpus.go
@@ -17,26 +17,24 @@ func (c *Corpus) WalkEnglishVolumes() <-chan *Volume {
 	// Spawn a goroutine for the tree walk.
 	go func() {
 
-		powerwalk.Walk(
-			c.path,
-			func(path string, info os.FileInfo, _ error) error {
+		powerwalk.Walk(c.path, func(path string, info os.FileInfo, _ error) error {
 
-				if info.IsDir() {
-					return nil
-				}
-
-				vol, err := NewVolumeFromPath(path)
-				if err != nil {
-					return err
-				}
-
-				if vol.IsEnglish() {
-					volumes <- vol
-				}
-
+			if info.IsDir() {
 				return nil
+			}
 
-			},
+			vol, err := NewVolumeFromPath(path)
+			if err != nil {
+				return err
+			}
+
+			if vol.IsEnglish() {
+				volumes <- vol
+			}
+
+			return nil
+
+		},
 		)
 
 		close(volumes)
