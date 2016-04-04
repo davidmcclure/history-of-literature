d00a2b9df1901860afeb65cb28c85bac8dedbf69
David McClure <davidwilliammcclure@gmail.com>
2016-04-04 16:02:35 -0700

    Back to Redis.

--- htrc/data/year_token_counts.py
+++ htrc/data/year_token_counts.py
@@ -3,17 +3,31 @@
 import shelve
 
 from multiprocessing import Pool
+from collections import defaultdict, Counter
 
+from htrc import config
+from htrc.data.keyset import Keyset
 from htrc.corpus import Corpus
 from htrc.volume import Volume
-from htrc.data.shelf import Shelf
 
 
 
-class YearTokenCounts(Shelf):
+class YearTokenCounts(Keyset):
 
 
-    def index(self, num_procs=8):
+    @classmethod
+    def from_env(cls):
+
+        """
+        Use the ENV-defined Redis database.
+
+        Returns: cls
+        """
+
+        return cls(config['redis']['year_token_count'])
+
+
+    def index(self, num_procs=8, cache_len=100):
 
         """
         Index total token counts by year.
@@ -22,13 +36,13 @@ class YearTokenCounts(Shelf):
             num_procs (int)
         """
 
-        # TODO: filter out infrequent words?
-
         corpus = Corpus.from_env()
+
+        cache = defaultdict(Counter)
 
         with Pool(num_procs) as pool:
 
-            # Job for each volume.
+            # Queue volume jobs.
             jobs = pool.imap_unordered(
                 worker,
                 corpus.paths(),
@@ -37,13 +51,35 @@ class YearTokenCounts(Shelf):
             # Accumulate the counts.
             for i, (year, counts) in enumerate(jobs):
 
-                if year in self.data:
-                    self.data[year] += counts
+                # TODO: frequency filter?
+                cache[year] += counts
 
-                else:
-                    self.data[year] = counts
+                # Flush to Redis.
+                if i % cache_len == 0:
+                    self.flush_cache(cache)
+                    cache.clear()
 
                 print(i)
+
+
+    def flush_cache(self, cache):
+
+        """
+        Flush a cache to the Redis.
+
+        Args:
+            cache (dict)
+        """
+
+        print('INCR {0}'.format(len(cache)))
+
+        pipe = self.redis.pipeline()
+
+        for year, counts in cache.items():
+            for token, count in counts.items():
+                pipe.hincrby(str(year), token, str(count))
+
+        pipe.execute()
 
 
 
@@ -56,9 +92,9 @@ def worker(path):
         path (str): A volume path.
 
     Returns:
-        tuple (year<str>, counts<Counter>)
+        tuple (year<int>, counts<Counter>)
     """
 
     vol = Volume(path)
 
-    return (str(vol.year), vol.cleaned_token_counts())
+    return (vol.year, vol.cleaned_token_counts())
