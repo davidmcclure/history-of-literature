73f506b6424878630df98718c831f037a6daa76e
David McClure <davidwilliammcclure@gmail.com>
2016-04-05 09:12:04 -0700

    Reset the Count table.

--- htrc/models/count.py
+++ htrc/models/count.py
@@ -17,63 +17,18 @@ class Count(Base):
 
     id = Column(Integer, primary_key=True)
 
-    token = Column(String, nullable=False, index=True)
+    token = Column(String, nullable=False)
 
-    year = Column(Integer, nullable=False, index=True)
+    year = Column(Integer, nullable=False)
 
     count = Column(Integer, nullable=False)
 
 
-    @classmethod
-    def index(self, num_procs=8):
 
-        """
-        Index token counts by year.
-
-        Args:
-            num_procs (int)
-        """
-
-        # TODO: filter out infrequent words?
-
-        corpus = Corpus.from_env()
-
-        with Pool(num_procs) as pool:
-
-            # Job for each volume.
-            jobs = pool.imap_unordered(
-                worker,
-                corpus.paths(),
-            )
-
-            # Accumulate the counts.
-            for i, (year, counts) in enumerate(jobs):
-                print(year, len(counts))
-
-
-
-# Unique index on the token-year pair.
+# Unique index token + year.
 Index(
     'ix_count_token_year',
     Count.token,
     Count.year,
     unique=True,
 )
-
-
-
-def worker(path):
-
-    """
-    Extract a token counts for a volume.
-
-    Args:
-        path (str): A volume path.
-
-    Returns:
-        tuple (year<int>, counts<Counter>)
-    """
-
-    vol = Volume(path)
-
-    return (vol.year, vol.cleaned_token_counts())
