f6408f8951f5c78b717532714b23e37b954efe11
David McClure <davidwilliammcclure@gmail.com>
2016-04-12 15:59:16 -0700

    Stub in a MockCorpus class.

--- test/helpers.py
+++ test/helpers.py
@@ -1,5 +1,7 @@
 
 
+import uuid
+
 from hol.page import Page
 from hol.volume import Volume
 
@@ -22,7 +24,7 @@ def make_page(counts):
     })
 
 
-def make_vol(counts):
+def make_vol(counts, id=None, year=1900, language='eng'):
 
     """
     Make a volume instance with the provided tokenPosCount maps.
@@ -33,10 +35,22 @@ def make_vol(counts):
     Returns: Volume
     """
 
+    if not id:
+        id = str(uuid.uuid4())
+
     data = {
+
+        'id': id,
+
+        'metadata': {
+            'pubDate': str(year),
+            'language': language,
+        },
+
         'features': {
             'pages': []
-        }
+        },
+
     }
 
     for c in counts:
---
+++ test/mock_corpus.py
@@ -0,0 +1,45 @@
+
+
+import os
+import tempfile
+import json
+
+from bz2 import compress
+
+
+class MockCorpus:
+
+
+    def __init__(self):
+
+        """
+        Create the temporary directory.
+        """
+
+        self.path = tempfile.mkdtemp()
+
+
+    def teardown(self):
+
+        """
+        Delete the temporary directory.
+        """
+
+        shutil.rmtree(self.path)
+
+
+    def add_vol(self, vol):
+
+        """
+        Add a volume to the corpus.
+
+        Args:
+            vol (Volume)
+        """
+
+        path = os.path.join(self.path, '{0}.json.bz2'.format(vol.id))
+
+        data = compress(json.dumps(vol.data).encode('utf8'))
+
+        with open(path, 'wb') as fh:
+            fh.write(data)
