63e73aff17d169c60d33722f78ca7ad785cc9241
David McClure <davidwilliammcclure@gmail.com>
2016-04-13 11:42:33 -0700

    Stubbing in the beginning of a Count.index() test.

--- test/models/count/test_index.py
+++ test/models/count/test_index.py
@@ -1,15 +1,43 @@
 
 
 import pytest
+import time
+
+from hol.models import Count
+
+from test.helpers import make_vol
 
 
 pytestmark = pytest.mark.usefixtures('db')
 
 
-def test_index(mock_corpus):
+def test_index(mock_corpus, config):
 
     """
     Count.index() should index per-year token counts.
     """
 
-    pass
+    for i in range(100):
+
+        vol = make_vol([
+            {
+                'one': {
+                    'POS': 1
+                },
+                'two': {
+                    'POS': 1
+                },
+                'three': {
+                    'POS': 1
+                },
+            },
+        ])
+
+        mock_corpus.add_vol(vol)
+
+    Count.index()
+
+    # TODO
+    session = config.Session()
+    print(session.query(Count).count())
+    assert False
