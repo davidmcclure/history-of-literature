e1a63156d17b4fd2f0d070e8865f28009f2cc5aa
David McClure <davidwilliammcclure@gmail.com>
2016-04-13 12:57:42 -0700

    Test year merging in Count.index().

--- test/models/count/test_index.py
+++ test/models/count/test_index.py
@@ -63,3 +63,40 @@ def test_index_year_token_counts(mock_corpus, config):
     assert Count.token_year_count('three',  1902) == 4
     assert Count.token_year_count('three',  1903) == 5
     assert Count.token_year_count('four',   1903) == 6
+
+
+def test_merge_year_counts(mock_corpus, config):
+
+    """
+    Token counts for the same years should be merged.
+    """
+
+    v1 = make_vol(year=1901, counts=[
+        {
+            'one': {
+                'POS': 1
+            },
+            'two': {
+                'POS': 2
+            },
+        },
+    ])
+
+    v2 = make_vol(year=1901, counts=[
+        {
+            'one': {
+                'POS': 11
+            },
+            'two': {
+                'POS': 12
+            },
+        },
+    ])
+
+    mock_corpus.add_vol(v1)
+    mock_corpus.add_vol(v2)
+
+    Count.index()
+
+    assert Count.token_year_count('one', 1901) == 1+11
+    assert Count.token_year_count('two', 1901) == 2+12
