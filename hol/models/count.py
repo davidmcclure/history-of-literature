eec6fcf8355077b1703b29d11008e7d4ad16bfd5
David McClure <davidwilliammcclure@gmail.com>
2016-04-20 16:41:41 -0700

    Move CountQueries methods onto Count.

--- hol/models/count.py
+++ hol/models/count.py
@@ -1,9 +1,12 @@
 
 
-from collections import defaultdict, Counter
+import numpy as np
 
-from sqlalchemy.schema import Index
+from collections import defaultdict, Counter, OrderedDict
+from functools import lru_cache
+
 from sqlalchemy import Column, Integer, String, PrimaryKeyConstraint
+from sqlalchemy.schema import Index
 from sqlalchemy.sql import text, func
 
 from hol import config
@@ -119,6 +122,7 @@ class Count(Base):
 
 
     @classmethod
+    @lru_cache()
     def token_year_count(cls, token, year):
 
         """
@@ -140,3 +144,201 @@ class Count(Base):
             )
 
             return res.scalar() or 0
+
+
+    @classmethod
+    @lru_cache()
+    def tokens(cls):
+
+        """
+        Get an ordered list of all tokens.
+
+        Returns: list<str>
+        """
+
+        with config.get_session() as session:
+
+            res = (
+                session
+                .query(cls.token)
+                .distinct()
+                .order_by(cls.token.asc())
+            )
+
+            return [r[0] for r in res]
+
+
+    @classmethod
+    @lru_cache()
+    def total_token_count(cls):
+
+        """
+        Get the total number of observed tokens.
+
+        Returns: int
+        """
+
+        with config.get_session() as session:
+
+            res = (
+                session
+                .query(func.sum(cls.count))
+            )
+
+            return res.scalar()
+
+
+    @classmethod
+    @lru_cache()
+    def year_count(cls, year):
+
+        """
+        How many tokens appeared in year X?
+
+        Args:
+            year (int)
+
+        Returns: int
+        """
+
+        with config.get_session() as session:
+
+            res = (
+                session
+                .query(func.sum(cls.count))
+                .filter(cls.year==year)
+            )
+
+            return res.scalar()
+
+
+    @classmethod
+    @lru_cache()
+    def year_count_series(cls, years):
+
+        """
+        Get total token counts for a set of years.
+
+        Args:
+            years (iter)
+
+        Returns: OrderedDict {year: count, ...}
+        """
+
+        with config.get_session() as session:
+
+            res = (
+                session
+                .query(cls.year, func.sum(cls.count))
+                .filter(cls.year.in_(years))
+                .group_by(cls.year)
+                .order_by(cls.year)
+            )
+
+            return OrderedDict(res.all())
+
+
+    @classmethod
+    @lru_cache()
+    def token_count_series(cls, token, years):
+
+        """
+        Get counts for a specific token for a set of years.
+
+        Args:
+            token (str)
+            years (iter)
+
+        Returns: OrderedDict {year: count, ...}
+        """
+
+        with config.get_session() as session:
+
+            res = (
+                session
+                .query(cls.year, func.sum(cls.count))
+                .filter(cls.token==token, cls.year.in_(years))
+                .group_by(cls.year)
+                .order_by(cls.year)
+            )
+
+            return OrderedDict(res.all())
+
+
+    @classmethod
+    @lru_cache()
+    def token_wpm_series(cls, token, years):
+
+        """
+        Get a WMP series for a token.
+
+        Args:
+            token (str)
+            years (iter)
+
+        Returns: OrderedDict {year: wpm, ...}
+        """
+
+        baseline = cls.year_count_series(years)
+
+        token_counts = cls.token_count_series(token, years)
+
+        series = OrderedDict()
+        for year, count in token_counts.items():
+            wpm = (1e6 * count) / baseline[year]
+            series[year] = wpm
+
+        return series
+
+
+    @classmethod
+    @lru_cache()
+    def token_wpm_series_smooth(cls, token, years, width=10):
+
+        """
+        Smooth the WMP series for a token.
+
+        Args:
+            token (str)
+            years (iter)
+            width (int)
+
+        Returns: OrderedDict {year: wpm, ...}
+        """
+
+        series = cls.token_wpm_series(token, years)
+
+        wpms = series.values()
+
+        smooth = np.convolve(
+            list(wpms),
+            np.ones(width) / width,
+            mode='same',
+        )
+
+        return OrderedDict(zip(series.keys(), smooth))
+
+
+    @classmethod
+    @lru_cache()
+    def token_counts_by_year(cls, year):
+
+        """
+        Get the counts for all tokens that appeared in year X.
+
+        Args:
+            year (int)
+
+        Returns: OrderedDict {token: count, ...}
+        """
+
+        with config.get_session() as session:
+
+            res = (
+                session
+                .query(cls.token, cls.count)
+                .filter(cls.year==year)
+                .order_by(cls.token.asc())
+            )
+
+            return OrderedDict(res.all())
